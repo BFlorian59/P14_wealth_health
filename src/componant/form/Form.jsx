896c173b9f03ef0053f31717d593817109befc69
BFlorian59 <florianbroeks59@gmail.com>
2023-04-21 16:29:25 +0200

    correction

--- src/componant/form/Form.jsx
+++ src/componant/form/Form.jsx
@@ -13,84 +13,80 @@ function Modalcreate() {
 //    function clear() {
 //         localStorage.clear()
 //    }
-
-   
     return(
        
-            <div className="container">
-                <h2>Create Employee</h2>
-                <form>
+        <div className="container">
+            <h2>Create Employee</h2>
+            <form>
                     
-                    <label htmlFor="first-name">First Name</label>
-                    <input onChange={(firstname)=>{
+                <label htmlFor="first-name">First Name</label>
+                <input onChange={(firstname)=>{
+                    setemployee({
+                        ...employee,
+                        firstname: firstname.currentTarget.value
+                    })
+                }} type="text" id="first-name" />
+
+                <label htmlFor="last-name">Last Name</label>
+                <input onChange={(lastname)=>{
+                    setemployee({
+                        ...employee,
+                        lastname: lastname.currentTarget.value
+                    })
+                }} type="text" id="last-name" />
+
+                <label htmlFor="date-of-birth">Date of Birth</label>
+                <DateInput selected={birthDate} onChange={(date) => setBirthDate(date)}/>
+
+                <label htmlFor="start-date">Start Date</label>
+                <DateInput selected={startDate} onChange={(date) => setStartDate(date)}/>
+                    
+                <fieldset className="address">
+                    <legend>Address</legend>
+
+                    <label htmlFor="street">Street</label>
+                    <input onChange={(street)=>{
                         setemployee({
                             ...employee,
-                            firstname: firstname.currentTarget.value
+                            street: street.currentTarget.value
                         })
-                    }} type="text" id="first-name" />
+                    }} id="street" type="text" />
 
-                    <label htmlFor="last-name">Last Name</label>
-                    <input onChange={(lastname)=>{
+                    <label htmlFor="city">City</label>
+                    <input onChange={(city)=>{
                         setemployee({
                             ...employee,
-                            lastname: lastname.currentTarget.value
+                            city: city.currentTarget.value
                         })
-                    }} type="text" id="last-name" />
+                    }} id="city" type="text" />
 
-                    <label htmlFor="date-of-birth">Date of Birth</label>
-                    <DateInput selected={birthDate} onChange={(date) => setBirthDate(date)}/>
+                    <label htmlFor="state">State</label>
+                    <State onChange={(state)=>{
+                        setemployee({
+                            ...employee,
+                            state: state.currentTarget.value
+                        })
+                    }}/>
 
-                    <label htmlFor="start-date">Start Date</label>
-                    <DateInput selected={startDate} onChange={(date) => setStartDate(date)}/>
-                    
-                    <fieldset className="address">
-                        <legend>Address</legend>
-
-                        <label htmlFor="street">Street</label>
-                        <input onChange={(street)=>{
-                            setemployee({
-                                ...employee,
-                                street: street.currentTarget.value
-                            })
-                        }} id="street" type="text" />
-
-                        <label htmlFor="city">City</label>
-                        <input onChange={(city)=>{
-                            setemployee({
-                                ...employee,
-                                city: city.currentTarget.value
-                            })
-                        }} id="city" type="text" />
-
-                        <label htmlFor="state">State</label>
-                        <State onChange={(state)=>{
-                            setemployee({
-                                ...employee,
-                                state: state.currentTarget.value
-                            })
-                        }}/>
-
-                        <label htmlFor="zip-code">Zip Code</label>
-                        <input onChange={(zip)=>{
-                            setemployee({
-                                ...employee,
-                                zip: zip.currentTarget.value
-                            })
-                        }} id="zip-code" type="number" />
-                    </fieldset>
-                    <Department onChange={(department)=>{
-                            setemployee({
-                                ...employee,
-                                department: department.currentTarget.value
-                            })
-                        }}/>
-                </form>
-                <button className="button-28" onClick={() => addEmployee()}>Save</button>
-                {/* <button className="button-28" onClick={() => clear()}>Clear</button> */}
-                {isOpen && <Modal setIsOpen={setIsOpen} modalText="Employee Created!" />}
-            </div>
-            
-        
+                    <label htmlFor="zip-code">Zip Code</label>
+                    <input onChange={(zip)=>{
+                        setemployee({
+                            ...employee,
+                            zip: zip.currentTarget.value
+                        })
+                    }} id="zip-code" type="number" />
+                </fieldset>
+                <Department onChange={(department)=>{
+                        setemployee({
+                            ...employee,
+                            department: department.currentTarget.value
+                        })
+                    }}/>
+            </form>
+            <button className="button-28" onClick={() => addEmployee()}>Save</button>
+            {/* <button className="button-28" onClick={() => clear()}>Clear</button> */}
+            {isOpen && <Modal setIsOpen={setIsOpen} modalText="Employee Created!" />}
+        </div>   
     )
 }
 export default Modalcreate
